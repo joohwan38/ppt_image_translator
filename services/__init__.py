aae5a0691db8e4491be76106e748d290d030fa56
joohwan.son <joohwan.son@linecorp.com>
2025-05-13 08:20:52 +0900

    코드 갈아엎기1

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,6 @@
 # services 패키지 초기화
 from .ollama_service import OllamaService
 from .document_analyzer import DocumentAnalyzer
-from .translator import TranslationService
+from .translation import TranslationService
 
 __all__ = ['OllamaService', 'DocumentAnalyzer', 'TranslationService']
